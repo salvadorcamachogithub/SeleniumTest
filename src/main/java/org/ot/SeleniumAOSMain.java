fb6cb6f5ba894078cfd5c15c892248d4534d481f
Salvador Camacho <scamacho@opentext.com>
2024-07-16 15:54:22 -0700

    Added All Project Files

---
+++ src/main/java/org/ot/SeleniumAOSMain.java
@@ -0,0 +1,12 @@
+package org.ot;
+
+/**
+ * Hello world!
+ *
+ */
+public class SeleniumAOSMain
+{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
